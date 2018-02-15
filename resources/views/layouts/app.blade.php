e5a874b76cd43555a1cc811f93145099e3c9ac53
littleWanderer <teateafb.tm@gmail.com>
2018-02-15 06:06:34 +0100

    Profile page structure

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,18 +14,22 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="description" content="">
+    <link rel="stylesheet" href= {{ URL::asset('css/main.css') }}>
 
-    <link href="./../../../public/css/main.css" type="text/css" rel="stylesheet">
-    <title>Lazy Club</title>
+    <title>Lazy Club @yield('title')</title>
 </head>
 <body>
 
+@section('page_top_picture')
+@show
 
+@section('sidebar')
+@show
 
+<script src={{ URL::asset('js/jquery-3.3.1.min.js') }}></script>
+<script src={{ URL::asset('js/bootstrap.min.js') }}></script>
+<script src={{ URL::asset('js/main.js') }}></script>
 
-<script type="text/javascript" src="./../../../public/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="./../../../public/js/jquery-3.3.1.min.js"></script>
-<script type="text/javascript" src="./../../../public/js/main.js"></script>
 
 </body>
 </html>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,8 +1,19 @@
 
-/**
- * Created by PhpStorm.
- * User: TEODORA
- * Date: 2/12/2018
- * Time: 4:33 AM
- */
 @extends ('layouts.app')
+
+@section('title', '| Profile')
+
+@section('page_top_picture')
+ @parent
+
+ <div id="page_top_picture"></div>
+@endsection
+
+
+@section('sidebar')
+ @parent
+
+ <div id="kk">This is appended to the master sidebar.</div>
+@endsection
+
+
