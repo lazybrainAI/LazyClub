39ebfd800173ded4251e15b004390ffae5872327
littleWanderer <teateafb.tm@gmail.com>
2018-02-16 03:54:20 +0100

    Chages to profile page added.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,9 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="description" content="">
-    <link rel="stylesheet" href= {{ URL::asset('css/main.css') }}>
+
+    <link rel="stylesheet" href= {{ URL::asset('css/bootstrap.min.css') }}>
+
 
     <title>Lazy Club @yield('title')</title>
 </head>
@@ -23,11 +25,14 @@
 @section('page_top_picture')
 @show
 
-@section('page_title_button')
+
+
+@section('main')
+
 @show
 
-@section('sidebar')
-@show
+
+
 
 <script src={{ URL::asset('js/jquery-3.3.1.min.js') }}></script>
 <script src={{ URL::asset('js/bootstrap.min.js') }}></script>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -6,29 +6,118 @@
 @section('page_top_picture')
  @parent
 
- <div id="page_top_picture">
 
+ <link rel="stylesheet" href= {{ URL::asset('css/main.css') }}>
+
+
+ <div id="page_top_picture">
+     <div class="container-fluid p_t_button">
+         <div class="row justify-content-center">
+             <div class="col-sm-3 col-xs-4" id="page_title">
+                 <h2>Club | LazyBrain</h2>
+             </div>
+         </div>
+     </div>
  </div>
 @endsection
 
 
-@section('page_title_button')
+
+
+
+
+@section('main')
  @parent
 
- <div class="container-fluid">
-  <div class="row justify-content-center">
-   <div class="col-4" id="page_title">
-    <h1>Club | LazyBrain</h1>
-   </div>
-  </div>
+ <div class="container-fluid sidebar">
+     <div class="row">
+         <div class="col-sm-3 col-md-2 d-none d-sm-block">
+            <ul id="sidebar-menu">
+                <li>
+                    <a href="">
+                        <h5>Home</h5>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <h5>Events</h5>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <h5>Projects</h5>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <h5>Review</h5>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <h5>Documents</h5>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <h5>Profile</h5>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <h5>Logout</h5>
+                    </a>
+                </li>
+
+            </ul>
+         </div>
+         <div class="col-sm-9 col-md-10  col-xs-12 profile_info"> <!--profile information-->
+            <div class="container profile_details_container" >
+                <div class="row no-gutters">
+                    <div class="col-sm-4">
+                        <h5 class="section_title">Profile</h5>
+                    </div>
+                    <div class="col-md-3 offset-md-5  profile_details"> <!--profile details-->
+                        <h6>Profile details:</h6>
+                        <br>
+                        <h6 class="h7">Join date/</h6>
+                        <h6 class="h7">Status/</h6>
+
+                        <h6 class="h7">Strength/</h6>
+
+                    </div>
+
+                </div>
+            </div>
+
+             <div class="container profile_img_container">
+                 <div class="row">
+                     <div class="col-md-2">
+                         <img class=" profile_img" src={{ URL::asset('img/teo.jpeg') }} />
+                     </div>
+                     <div class="col-md-10"></div>
+                 </div>
+             </div>
+
+         </div>
+
+
+
+     </div>
  </div>
 
- @endsection
+@endsection
+
 
 
 @section('sidebar')
  @parent
 
- @endsection
+@endsection
+
+
+
+
+
 
 
