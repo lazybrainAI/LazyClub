1e8294785d09fea079e1561c6b90233315f2bb6e
KattieM <kattysmiley@gmail.com>
2018-03-05 00:01:14 +0100

    --User authentication

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Input;
+
+use App\User;
 
 class LoginController extends Controller
 {
@@ -17,23 +23,42 @@ class LoginController extends Controller
     | to conveniently provide its functionality to your applications.
     |
     */
-
-    use AuthenticatesUsers;
-
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+//
+//    use AuthenticatesUsers;
+//
+//    /**
+//     * Where to redirect users after login.
+//     *
+//     * @var string
+//     */
+//    protected $redirectTo = '/home';
+//
+//    /**
+//     * Create a new controller instance.
+//     *
+//     * @return void
+//     */
+//    public function __construct()
+//    {
+//        $this->middleware('guest')->except('logout');
+//    }
+    public function getLogin()
     {
-        $this->middleware('guest')->except('logout');
+        return view('login');
     }
+    public function postLogin(Request $request){
+
+
+        $userValid = $request->validate(['email'=>'required|email', 'password' => 'required|min:6']);
+
+
+        $user = User::where([['email', '=', Input::get('email')], ['password', '=', Input::get('password')]])->first();
+        if ($user && $userValid) {
+            return redirect('/home');
+        }
+
+        else
+            return redirect('/');
+
+}
 }
