02f76cfe1a3d057ffb0401b3e645abffe250239d
KattieM <kattysmiley@gmail.com>
2018-05-20 22:06:47 +0200

    --hamburger menu

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -22,7 +22,7 @@
  <div class="container-fluid sidebar_section">
 
      <div class="row">
-         <div class="col-sm-3 col-md-2 d-none d-sm-block">
+         <div class="col-sm-3 col-md-2 col-5 col-xs-5 d-sm-block">
            @include ('/php/sidebar_menu')
          </div>
          <div class="col-sm-9 col-md-10  col-xs-12 main_content_section">
